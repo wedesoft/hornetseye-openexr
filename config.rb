55b565ce22c6c42bec75779b009d5343fcf209be
Jan Wedekind <jan.wedekind@roke.co.uk>
2025-06-30 11:11:44 +0100

    Update link to homepage

--- config.rb
+++ config.rb
@@ -19,4 +19,4 @@ DESCRIPTION = %q{This Ruby extension provides reading and writing high dynamic r
 LICENSE = 'GPL-3+'
 AUTHOR = %q{Jan Wedekind}
 EMAIL = %q{jan@wedesoft.de}
-HOMEPAGE = %q{http://wedesoft.github.com/hornetseye-openexr/}
+HOMEPAGE = %q{http://wedesoft.github.io/hornetseye-openexr/}
